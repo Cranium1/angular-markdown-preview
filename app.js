7599dfe39607274a5712b703a6f517c4827a6568
Cranium1 <carson@ccrane.me>
2015-09-26 00:46:10 -0400

    Finish Demo

---
+++ app.js
@@ -0,0 +1,14 @@
+var BlogApp = angular.module('BlogApp', []);
+
+BlogApp.controller('PreviewController', function($scope) {
+	$scope.$watch('input', function(val) {
+		$scope.output = markdown.toHTML(val);
+	}, true);
+
+	$scope.show = false;
+
+	$scope.input = ""
+	$scope.output = ""
+});
+
+BlogApp.filter('html', function($sce) { return $sce.trustAsHtml; });
